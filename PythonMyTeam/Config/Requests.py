2ab6e4462b87e8a85674e4542cd265c05cc09acd
haobin12358 <1276121237@qq.com>
2017-12-17 14:56:49 +0800

    控制层注册函数及对应的request

--- PythonMyTeam/Config/Requests.py
+++ PythonMyTeam/Config/Requests.py
@@ -7,3 +7,39 @@ apis_wrong = {
     "status_code": 405099,
     "messages": "The API is not registered !"
 }
+
+register_ok = {
+    "status":200,
+    "messages":"register is ok !"
+}
+
+system_error = {
+    "status":404,
+    "status_code":404100,
+    "messages":"System is abnormal !"
+}
+
+repeated_name = {
+    "status":405,
+    "status_code":405100,
+    "messages":"Username is occupied !"
+}
+
+wrong_upwd = {
+    "status":405,
+    "status_code":405101,
+    "messages":"The password is not valid !"
+}
+
+wrong_uname = {
+    "status":405,
+    "status_code":405102,
+    "messages":"The username is not valid !"
+}
+
+param_miss = {
+    "status":405,
+    "status_code":405201,
+    "messages":"missing some parameters !"
+}
+
--- PythonMyTeam/control/CUsers.py
+++ PythonMyTeam/control/CUsers.py
@@ -1,12 +1,36 @@
 # *- coding:utf8 *-
 #引用python类
 from flask import request
+import json
+import uuid
 #引用项目类
-
+from common.JudgeData import JudgeData
+from service.SUsers import SUsers
+from Config.Requests import register_ok,system_error,repeated_name,wrong_uname,wrong_upwd,param_miss
 #用于处理用户相关的数据
 class CUsers():
     def login(self):
         return
 
     def register(self):
-        return
+
+        form = request.data
+        print str(form)
+        if str(form) == "" or str(form) == "[]":
+            return param_miss
+        form = json.loads(form)
+
+        judgeData = JudgeData()
+        if not judgeData.inData("Uname",form) or not judgeData.inData("Upwd",form) \
+            or not judgeData.inData("Utype", form):
+            return param_miss
+        susers = SUsers()
+
+        is_register = susers.register(uuid.uuid4(),form["Uname"],form["Upwd"],form["Utype"])
+
+        if is_register == 0:
+            return register_ok
+        elif is_register == 1:
+            return system_error
+        elif is_register == 2:
+            return repeated_name
