e3e789d3aa92c244116afb011713652637a27e55
shuaibinliu <1358804615@163.com>
2017-12-17 14:13:38 +0800

    model

--- PythonMyTeam/models/model.py
+++ PythonMyTeam/models/model.py
@@ -1,9 +1,15 @@
 # -*- coding:utf-8-*-
 #引用python类
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Index, Integer, String, UniqueConstraint,Text
+from sqlalchemy import Column, create_engine, Integer, String, Text
+from Config import dbconfig as cfg
+#获取和mysql的连接引擎格式 "数据库://用户名:密码@ip(:端口号)/databse名(?charset=字符集)" ()里是可选内容
+DB_PARAMS= "{0}://{1}:{2}@{3}/{4}?charset={5}".format(
+    cfg.sqlenginename,cfg.username,cfg.password,cfg.host,cfg.database,cfg.charset)
+mysql_engine = create_engine(DB_PARAMS,echo=True)
 
-Base = declarative_base() #实例化
+#实例化基础表，这个这个基础类可以关联到数据库的具体字段
+Base = declarative_base()
 
 #用户表
 class Uers(Base):
@@ -62,11 +68,13 @@ class Teams(Base):
 #团队教师关联表
 class TTeacher(Base):
     __tablename__ = "TTeacher"
+    TTid = Column(String(64),primary_key=True)
     TEid = Column(String(64),nullable=False)
     Tid = Column(String(64),nullable=False)
 
 #学生技能表
 class STechs(Base):
+    __tablename__ = "STechs"
     STid = Column(String(64),primary_key=True)
     Sid = Column(String(64),nullable=False)
     STname = Column(String(32),nullable=False)
@@ -74,16 +82,23 @@ class STechs(Base):
 
 #学生竞赛简历
 class SCuse(Base):
+    __tablename__ = 'SCuse'
     SCid = Column(String(64),primary_key=True)
     Sid = Column(String(64),nullable=False)
     SCname = Column(String(128),nullable=False)
     SCno = Column(String(16),nullable=False)
 
 #教师竞赛简历
-class Tcuse(Base):
+class TCuse(Base):
+    __tablename__ = 'TCuse'
     TCid = Column(String(64),primary_key=True)
     Tid = Column(String(64),nullable=False)
     TCname = Column(String(128),nullable=False)
     TCon = Column(String(16),nullable=False)
     TCnum = Column(Integer,nullable=False,default=1)
 
+if __name__ == "__main__":
+    '''
+    运行该文件就可以在对应的数据库里生成本文件声明的所有table
+    '''
+    Base.metadata.create_all(mysql_engine)
