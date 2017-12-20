ee83ebcb9008e41d9e7f85c9e4dd091d0ec5a5c7
haobin12358 <1276121237@qq.com>
2017-12-20 18:38:56 +0800

    教师查询方法、学生查询方法、学生控制方法

--- PythonMyTeam/control/CStudents.py
+++ PythonMyTeam/control/CStudents.py
@@ -1,12 +1,44 @@
 # *- coding:utf8 *-
 #引用Python类
 from flask import request
+import json
 #引用项目类
-
+from common.JudgeData import JudgeData
+from service.SStudents import SStudents
+from Config.Requests import param_miss, system_error
 #用于处理学生信息相关的数据
 class CStudents():
+    def __init__(self):
+        self.judgeData = JudgeData()
+        self.sstudents = SStudents() #实例化
+
     def students_list(self):
-        pass
+        args = request.args
+        print str(args)
+        #判断是否含有参数
+        if str(args) == "" or str(args) == {}:
+            students_list = self.sstudents.get_students_list()
+            if students_list is not None:
+                return students_list
+            else:
+                return []
+        args = json.loads(args)
+
+        #参数成对存在，判断是否缺失
+        if not self.judgeData.inData("page_no", args) or not self.judgeData.inData("infor_num", args):
+            return param_miss
+
+        page_no = args["page_no"]
+        infor_num = args["infor_num"]
+
+        start_num = (page_no - 1) * infor_num
+
+        students_list = self.sstudents.get_students_list_by_start_end(start_num, infor_num)
+
+        if students_list is not None:
+            return students_list#返回体现在很有问题
+        else:
+            return system_error
 
     def students_abo(self):
         pass
---
+++ PythonMyTeam/service/STeachers.py
@@ -0,0 +1,40 @@
+# *- coding:utf8 *-
+#引用项目类
+from common.TransformToList import trans_params
+from models import model
+from DBSession import db_session
+
+#操作teachers表的相关方法
+class STeachers():
+    def __init__(self):
+        try:
+            self.session = db_session()#实例化
+            self.status = True #session异常的判断标记
+        except Exception as e:
+            print e.message
+            self.status = False
+
+    #获取全部教师信息列表
+    def get_teachers_list(self):
+        teachers_list = self.session.query(model.Teachers.Tid, model.Teachers.Tname, model.Teachers.Tschool,
+                                           model.Teachers.Ttime).all()
+        return teachers_list
+
+    #分页获取教师信息列表
+    def get_teachers_list_by_start_end(self, start_num, infor_num):
+        teachers_list = self.session.query(model.Teachers.Tid, model.Teachers.Tname, model.Teachers.Tschool,
+                                           model.Teachers.Ttime).offset(start_num - 1).limit(infor_num).all()
+        return teachers_list
+
+    #根据tid获取教师信息详情
+    def get_teacher_abo_by_tid(self, tid):
+        teacher_abo = self.session.query(model.Teachers.Tid, model.Teachers.Uid, model.Teachers.Tname,
+                                         model.Teachers.Ttel, model.Teachers.Tno, model.Teachers.Tuniversity,
+                                         model.Teachers.Tschool, model.Teachers.Ttime).all()
+        return teacher_abo
+
+    #获取教师带队经历
+    def get_teacher_use_by_tid(self, tid):
+        teacher_use = self.session.query(model.TCuse.Tid, model.TCuse.TCname, model.TCuse.TCon, model.TCuse.TCnum)\
+            .filter_by(Tid = tid).all()
+        return teacher_use
