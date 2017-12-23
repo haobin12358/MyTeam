81e4a222dafefd2375fe3d4f70864f26f5d8e467
haobin12358 <1276121237@qq.com>
2017-12-23 20:55:51 +0800

    创建personal的服务

--- PythonMyTeam/control/CPersonal.py
+++ PythonMyTeam/control/CPersonal.py
@@ -2,13 +2,21 @@
 #引用python类
 from flask import request
 #引用项目类
+from common.JudgeData import JudgeData
+from service.SPersonal import SPersonal
+from Config.Requests import param_miss,system_error
 
 #用于处理个人信息相关数据
 class CPersonal():
+    def __init__(self):
+        self.judgeData = JudgeData()
+        self.spersonal = SPersonal()
+
     def myinfor(self):
         pass
 
     def create_myinfor(self):
+
         pass
 
     def create_mytech(self):
