05bbf6fd7089d8a3553ce1d1993c69607d9b6bbf
shuaibinliu <1358804615@163.com>
2017-12-23 20:59:20 +0800

    session public method

--- PythonMyTeam/service/DBSession.py
+++ PythonMyTeam/service/DBSession.py
@@ -1,9 +1,21 @@
 # *- coding:utf8 *-
-#引用python类
+# 引用python类
 from sqlalchemy.orm import sessionmaker
-#引用项目类
+# 引用项目类
 from models import model
 
-#实例化session
+# 实例化session
 db_session = sessionmaker(bind=model.mysql_engine)
 
+
+# 获取数据库连接session
+def get_session():
+    try:
+        session = db_session()
+        status = True
+    except Exception as e:
+        print e.message
+        session = None
+        status = False
+    finally:
+        return session, status
