4a618e8bae7a7e12710afcd2436087470008f4ee
haobin12358 <1276121237@qq.com>
2018-01-05 11:37:12 +0800

    配置流程

---
+++ PythonPackage/前端配置.py
@@ -0,0 +1,13 @@
+# *- coding:utf8 *-
+# 先安装node.js，并默认配置好全局变量
+# 启动cmd命令行
+# npm install -g cnpm --registry=https://registry.npm.taobao.org
+# 配置好淘宝的cnpm镜像，这样运行快很多
+# cnpm install
+# 项目安装
+# 将目录下的build.zip文件解压到web目录下
+# npm install --save vue-resource
+# 配置vue-resource版本
+# npm run dev
+# 运行
+# 浏览器中输入localhost:8080启动项目
