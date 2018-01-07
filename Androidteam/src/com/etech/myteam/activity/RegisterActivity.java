0fc987077e1d696e2cbd51056bcc26502064b426
haobin12358 <1276121237@qq.com>
2018-01-07 19:58:05 +0800

    注册功能联调成功

--- Androidteam/src/com/etech/myteam/activity/RegisterActivity.java
+++ Androidteam/src/com/etech/myteam/activity/RegisterActivity.java
@@ -13,6 +13,7 @@ import com.etech.myteam.common.HttppostEntity;
 import com.etech.myteam.global.AppConst;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -35,11 +36,14 @@ public class RegisterActivity extends Activity{
 	private RadioButton rb1,rb2;
 	private Button btn1;
 	
+	private String Uname, Upwd;
+	private int Utype;
+	
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
 		init();
-		getText();
+		
 		btn1.setOnClickListener(register_button);
 	}
 	
@@ -56,8 +60,29 @@ public class RegisterActivity extends Activity{
 	}
 	
 	//获取填写的内容
-	private void getText(){
-		
+	private boolean getText(){
+		Uname = et1.getText().toString();
+		Log.e("Uname", Uname);
+		if(Uname.length() == 0){
+			new AlertDialog.Builder(this).setTitle(R.string.ti_xing).setMessage(R.string.qing_shu_ru_yong_hu_ming).show();
+			return false;
+		}
+		Upwd = et2.getText().toString();
+		Log.e("Upwd", Upwd);
+		if(Upwd.length() == 0){
+			new AlertDialog.Builder(this).setTitle(R.string.ti_xing).setMessage(R.string.qing_shu_ru_mi_ma).show();
+			return false;
+		}
+		int Utype;
+		if(rb1.isChecked()){
+			Utype = 100;
+		}else if(rb2.isChecked()){
+			Utype = 101;
+		}else{
+			new AlertDialog.Builder(this).setTitle(R.string.ti_xing).setMessage(R.string.qing_xuan_ze_lei_xing).show();
+			return false;
+		}
+		return true;
 	}
 	
 	private String result_register;
@@ -65,9 +90,9 @@ public class RegisterActivity extends Activity{
 	private void postText(){
 		JSONObject obj = new JSONObject();
 		try {
-			obj.put("Uname","123");
-			obj.put("Upwd", "123");
-			obj.put("Utype", 100);
+			obj.put("Uname",Uname);
+			obj.put("Upwd", Upwd);
+			obj.put("Utype", Utype);
 		} catch (JSONException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -89,12 +114,13 @@ public class RegisterActivity extends Activity{
 		@Override
 		public void onClick(View arg0) {
 			// TODO Auto-generated method stub
-			new Thread(){
-				public void run(){
-					postText();
-				}
-			}.start();
-			
+			if(getText()){
+				new Thread(){
+					public void run(){
+						postText();
+					}
+				}.start();
+			}
 			Log.e("register","ok");
 		}
 		
