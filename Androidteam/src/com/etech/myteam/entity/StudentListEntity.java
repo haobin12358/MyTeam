19336025c5bc8fed828d95d67f19e6a63929be62
shuaibinliu <1358804615@163.com>
2018-01-12 23:34:25 +0800

    infor fragment

---
+++ Androidteam/src/com/etech/myteam/entity/StudentListEntity.java
@@ -0,0 +1,33 @@
+package com.etech.myteam.entity;
+
+public class StudentListEntity {
+	private String id;
+	private String name;
+	private String school;
+	private int grade;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
+}
