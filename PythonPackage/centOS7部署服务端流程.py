7973bd0f13c02607588b35c76a93ab78192df5c6
haobin12358 <1276121237@qq.com>
2018-01-17 15:04:42 +0800

    更新配置流程，已经完成后台启动+nginx+uwsgi的配置

--- PythonPackage/centOS7部署服务端流程.py
+++ PythonPackage/centOS7部署服务端流程.py
@@ -36,3 +36,75 @@ exit; # 退出
 # 通过pip安装即可
 
 # linux安装
+# 2.安装uwsgi
+yum install -y pcre pcre-devel pcre-static # 预安装基础组件
+pip install uwsgi # 安装
+
+# 2-1.项目根目录下配置config.ini
+[uwsgi]
+socket = 127.0.0.1:8001     #启动程序时所使用的地址和端口，通常在本地运行flask项目，
+                            #地址和端口是127.0.0.1:5000,
+                            #不过在服务器上是通过uwsgi设置端口，通过uwsgi来启动项目，
+                            #也就是说启动了uwsgi，也就启动了项目。
+chdir = /home/www/     #项目目录
+wsgi-file = manage.py      #flask程序的启动文件，通常在本地是通过运行  
+                           #python manage.py runserver 来启动项目的
+callable = app      #程序内启用的application变量名
+processes = 4     #处理器个数
+threads = 2     #线程个数
+stats = 127.0.0.1:9191      #获取uwsgi统计信息的服务地址
+
+# 2-2.启动uwsgi
+uswgi config.ini
+
+# 3.安装nginx
+rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
+yum install nginx
+
+# 3-1 nginx的一些命令
+nginx # 启动
+nginx -s stop # 停止
+pkill -9 nginx # 强行停止
+nginx -t # 查看是否应用配置文件
+nginx -v # 查看nginx版本
+
+# 3-2 nginx配置文件，在/etc/nginx目录下，名字为nginx.conf
+
+events {
+    worker_connections  1024;
+}
+http {
+    include       mime.types;    
+    default_type  application/octet-stream;    
+    sendfile        on;    
+    keepalive_timeout  65;
+
+    server {
+        listen       80;# 默认的web访问端口
+        server_name  xxx.xxx.xxx.xxx;# 你的公网ip
+        #charset koi8-r;
+        access_log  /home/www/WebBlogold/logs/access.log;# 服务器接收的请求日志，
+                                                         # 需要在项目文件夹下创建
+                                                         # logs文件夹，下同。
+        error_log  /home/www/WebBlogold/logs/error.log;  # 错误日志
+        location / {
+            include        uwsgi_params;# 这里是导入的uwsgi配置
+
+            uwsgi_pass     127.0.0.1:8001;# 需要和uwsgi的配置文件里socket项的地址
+                                          # 相同,否则无法让uwsgi接收到请求。
+            uwsgi_param UWSGI_PYHOME /home/www/WebBlogold/venv;# python的位置(虚拟环境下)
+            uwsgi_param UWSGI_CHDIR  /home/www/WebBlogold;# 项目根目录
+            uwsgi_param UWSGI_SCRIPT manage:app;# 启动项目的主程序(在本地上运行
+                                                # 这个主程序可以在flask内置的
+                                                # 服务器上访问你的项目)
+
+        }
+    }
+}
+
+# 3-3 查看端口占用情况
+netstat -ntlp
+
+
+# 3-4 后台启动
+nohup uswgi /opt/PythonMyTeam/config.ini &
