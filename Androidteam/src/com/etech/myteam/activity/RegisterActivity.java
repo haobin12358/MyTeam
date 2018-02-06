ae400013b8c99ef68d95aa3f46b404357a1b057e
haobin12358 <1276121237@qq.com>
2018-02-06 21:55:32 +0800

    修复注册的bug

--- Androidteam/src/com/etech/myteam/activity/RegisterActivity.java
+++ Androidteam/src/com/etech/myteam/activity/RegisterActivity.java
@@ -26,6 +26,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends Activity{
 	
@@ -37,7 +38,7 @@ public class RegisterActivity extends Activity{
 	private EditText et1, et2;
 	private RadioGroup rg;
 	private RadioButton rb1,rb2;
-	private Button btn1;
+	private Button btn1, btn2;
 	
 	private String Uname, Upwd;
 	private int Utype;
@@ -59,11 +60,13 @@ public class RegisterActivity extends Activity{
 		rb1 = (RadioButton)findViewById(R.id.rb_1);
 		rb2 = (RadioButton)findViewById(R.id.rb_2);
 		btn1 = (Button)findViewById(R.id.btn_1);
+		btn2 = (Button)findViewById(R.id.btn_2);
 	}
 	
 	//设置响应事件
 	private void setListener(){
 		btn1.setOnClickListener(register_button);
+		btn2.setOnClickListener(back);
 	}
 	
 	//获取填写的内容
@@ -80,7 +83,6 @@ public class RegisterActivity extends Activity{
 			new AlertDialog.Builder(this).setTitle(R.string.ti_xing).setMessage(R.string.qing_shu_ru_mi_ma).show();
 			return false;
 		}
-		int Utype;
 		if(rb1.isChecked()){
 			Utype = 100;
 		}else if(rb2.isChecked()){
@@ -101,6 +103,7 @@ public class RegisterActivity extends Activity{
 			obj.put("Uname",Uname);
 			obj.put("Upwd", Upwd);
 			obj.put("Utype", Utype);
+			Log.e("Utype", Integer.toString(Utype));
 		} catch (JSONException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -124,58 +127,70 @@ public class RegisterActivity extends Activity{
 		public void onClick(View arg0) {
 			// TODO Auto-generated method stub
 			if(getText()){
+				getText();
 				new Thread(){
 					public void run(){
 						postText();
 					}
 				}.start();
-				JSONObject json_obj = StringToJSON.toJSONObject(result_register);
-				if(json_obj.optInt("status") == 200){
+				try {
+					Thread.sleep(200);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				if(result_register == null){
 					new AlertDialog.Builder(RegisterActivity.this)
 						.setTitle(R.string.ti_xing)
-						.setMessage(R.string.zhu_ce_cheng_gong)
-						.setPositiveButton(R.string.que_ding, (android.content.DialogInterface.OnClickListener) positive)
+						.setMessage(R.string.xi_tong_yi_chang)
 						.show();
-					
-				}else if(json_obj.optInt("status") == 404){
-					new AlertDialog.Builder(RegisterActivity.this)
-					.setTitle(R.string.ti_xing)
-					.setMessage(R.string.xi_tong_yi_chang)
-					.show();
-				}else if(json_obj.optInt("status") == 405){
-					//根据不同的返回码提示不同内容
-					if(json_obj.optInt("status_code") == 405100){
-						//用户名重复
+				}else{
+					JSONObject json_obj = StringToJSON.toJSONObject(result_register);
+					if(json_obj.optInt("status") == 200){
+						Toast.makeText(RegisterActivity.this, "注册成功", Toast.LENGTH_SHORT);
+						Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+						startActivity(intent);
+						finish();
+					}else if(json_obj.optInt("status") == 404){
 						new AlertDialog.Builder(RegisterActivity.this)
 							.setTitle(R.string.ti_xing)
-							.setMessage(R.string.yong_hu_ming_chong_fu)
+							.setMessage(R.string.xi_tong_yi_chang)
 							.show();
-					}else if(json_obj.optInt("status_code") == 405101){
-						//密码不合法
+					}else if(json_obj.optInt("status") == 405){
+						//根据不同的返回码提示不同内容
+						if(json_obj.optInt("status_code") == 405100){
+							//用户名重复
+							new AlertDialog.Builder(RegisterActivity.this)
+								.setTitle(R.string.ti_xing)
+								.setMessage(R.string.yong_hu_ming_chong_fu)
+								.show();
+						}else if(json_obj.optInt("status_code") == 405101){
+							//密码不合法
+							new AlertDialog.Builder(RegisterActivity.this)
+								.setTitle(R.string.ti_xing)
+								.setMessage(R.string.mi_ma_bu_he_fa)
+								.show();
+						}else if(json_obj.optInt("status_code") == 405102){
+							//用户名不合法
+							new AlertDialog.Builder(RegisterActivity.this)
+								.setTitle(R.string.ti_xing)
+								.setMessage(R.string.yong_hu_ming_bu_he_fa)
+								.show();
+						}
+					}else{
 						new AlertDialog.Builder(RegisterActivity.this)
 							.setTitle(R.string.ti_xing)
-							.setMessage(R.string.mi_ma_bu_he_fa)
-							.show();
-					}else if(json_obj.optInt("status_code") == 405102){
-						//用户名不合法
-						new AlertDialog.Builder(RegisterActivity.this)
-							.setTitle(R.string.ti_xing)
-							.setMessage(R.string.yong_hu_ming_bu_he_fa)
+							.setMessage(R.string.wei_zhi_cuo_wu)
 							.show();
 					}
-				}else{
-					new AlertDialog.Builder(RegisterActivity.this)
-						.setTitle(R.string.ti_xing)
-						.setMessage(R.string.wei_zhi_cuo_wu)
-						.show();
+					Log.e("register","ok");
 				}
-				Log.e("register","ok");
+				
 			}
 		}
 	};
-	
-	//设置弹出框后的处理方式
-	private OnClickListener positive = new OnClickListener(){
+
+	private OnClickListener back = new OnClickListener(){
 
 		@Override
 		public void onClick(View arg0) {
