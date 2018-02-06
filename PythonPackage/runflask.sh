499dc78da6ac68776ffa621e09b10655098fd803
haobin12358 <1276121237@qq.com>
2018-02-06 18:00:27 +0800

    完成stop功能

--- PythonPackage/runflask.sh
+++ PythonPackage/runflask.sh
@@ -2,8 +2,24 @@ DATATIME=$(date +%Y%m%d)
 LOGSTART="/opt/logs/error/error"
 LOGEND=".log"
 LOGNAME=$LOGSTART$DATATIME$LOGEND
+NAME="uwsgi"
 start()
 {
+  echo $"stoping uwsgi...."
+  if [ ! -n "$NAME" ];then
+    echo "no arguments"
+    exit;
+  fi
+  echo $NAME
+  ID=`ps -ef | grep uwsgi | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
+  echo $ID
+  echo "#####################################"
+  for id in $ID
+  do
+  kill -9 $id
+  echo "success kill $id"
+  done
+  echo "####################################"
   echo $"starting run uwsgi....."
   echo LOGNAME
   if [ ! -f "$LOGNAME" ]; then
